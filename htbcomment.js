66396589c09d85bcd5224503ec062928bd78c41c
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2011-07-08 00:37:23 +0900

    untabify

--- htbcomment.js
+++ htbcomment.js
@@ -1,21 +1,16 @@
 $(document).ready(function() {
   chrome.tabs.getSelected(window.id, function (tab) {
-//     $("#dump").append($.dump(tab));
     var url = "http://b.hatena.ne.jp/entry/jsonlite/" + tab.url;
-// 	$("#dump").append($.dump(url));
+//  $("#dump").append($.dump(url));
     $.getJSON(url, function(json){
-// 	$("#dump").append($.dump(json));
     var j = 0;
       for (var i in json.bookmarks) {
-	    var b = json.bookmarks[i];
+        var b = json.bookmarks[i];
         if (! b.comment) continue;
         $("#comments").append(b.comment + '<br />');
-		j++;
-		if (j > 20) break;
-//            $("#dump").append($.dump(b));
-	  }
+        j++;
+        if (j > 20) break;
+      }
     });
   });
 });
-
-

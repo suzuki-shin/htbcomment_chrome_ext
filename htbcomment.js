0aa5c145e31a620196e35bfcd0bc8e71967bcca3
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2011-07-08 00:35:35 +0900

    はてぶコメントが表示されるようになった

--- htbcomment.js
+++ htbcomment.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
-  var url = "http://b.hatena.ne.jp/entry/jsonlite/" + "http://semooh.jp/jquery/api/ajax/jQuery.ajax/options/";
-  $.getJSON(url, function(json){
-// 	$("#dump").append($.dump(json.bookmarks));
-    for (var i in json.bookmarks) {
-	  var b = json.bookmarks[i];
-      if (! b.comment) continue;
-      $("#comments").append(b.comment + '<br />');
-	}
+  chrome.tabs.getSelected(window.id, function (tab) {
+//     $("#dump").append($.dump(tab));
+    var url = "http://b.hatena.ne.jp/entry/jsonlite/" + tab.url;
+// 	$("#dump").append($.dump(url));
+    $.getJSON(url, function(json){
+// 	$("#dump").append($.dump(json));
+    var j = 0;
+      for (var i in json.bookmarks) {
+	    var b = json.bookmarks[i];
+        if (! b.comment) continue;
+        $("#comments").append(b.comment + '<br />');
+		j++;
+		if (j > 20) break;
+//            $("#dump").append($.dump(b));
+	  }
+    });
   });
 });
+
+

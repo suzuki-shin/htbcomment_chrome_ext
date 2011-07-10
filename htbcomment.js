dbf7c3b169e313cb0eaa839298ad73c386431180
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2011-07-10 15:55:15 +0900

    READMEにこれが何かを書いてみた

--- htbcomment.js
+++ htbcomment.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
+  var comment_num = 0;
   chrome.tabs.getSelected(window.id, function (tab) {
+//      $("#dump").append($.dump(tab));
     var url = "http://b.hatena.ne.jp/entry/jsonlite/" + tab.url;
-//  $("#dump").append($.dump(url));
     $.getJSON(url, function(json){
-    var j = 0;
       for (var i in json.bookmarks) {
         var b = json.bookmarks[i];
         if (! b.comment) continue;
         $("#comments").append(b.comment + '<br />');
-        j++;
-        if (j > 20) break;
+        comment_num++;
+        if (comment_num> 20) break;
       }
     });
   });

bfe53da01a74d4b68a5a95aa209fc3842df7045d
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2011-07-13 23:30:28 +0900

    localStorageにキャッシュするようにした

--- htbcomment.js
+++ htbcomment.js
@@ -1,16 +1,30 @@
-$(document).ready(function() {
+function displayComment(bookmarks) {
   var comment_num = 0;
+  for (var i in bookmarks) {
+    var b = bookmarks[i];
+    if (! b.comment) continue;
+    $("#comments").append(b.comment + '<br />');
+    comment_num++;
+    if (comment_num> 20) break;
+  }
+}
+
+$(document).ready(function() {
   chrome.tabs.getSelected(window.id, function (tab) {
-//      $("#dump").append($.dump(tab));
-    var url = "http://b.hatena.ne.jp/entry/jsonlite/" + tab.url;
-    $.getJSON(url, function(json){
-      for (var i in json.bookmarks) {
-        var b = json.bookmarks[i];
-        if (! b.comment) continue;
-        $("#comments").append(b.comment + '<br />');
-        comment_num++;
-        if (comment_num> 20) break;
-      }
-    });
+    if (! localStorage[tab.url]) {
+      var url = "http://b.hatena.ne.jp/entry/jsonlite/" + tab.url;
+     $("#dump").append('api access...');
+      console.log('api access...');
+      $.getJSON(url, function(json){
+        localStorage[tab.url] = JSON.stringify(json); // set cache
+//      $("#dump").append($.dump(json));
+        displayComment(json.bookmarks);
+      });
+    }
+    else {
+      var json = JSON.parse(localStorage[tab.url]); // get cache
+      console.log(json);
+      displayComment(json.bookmarks);
+    }
   });
 });

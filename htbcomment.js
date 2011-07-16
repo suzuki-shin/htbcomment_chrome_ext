3ceb1a81dc41d990ecd4d220ee08a3c543741a93
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2011-07-16 14:18:58 +0900

    キャッシュのexpireをつけるようにした

--- htbcomment.js
+++ htbcomment.js
@@ -1,3 +1,7 @@
+var cacheHour = 10;
+var cacheMSec = cacheHour * 60 * 60 * 1000;
+// var cacheMSec = 30000;
+
 function displayComment(bookmarks) {
   var comment_num = 0;
   for (var i in bookmarks) {
@@ -10,21 +14,29 @@ function displayComment(bookmarks) {
 }
 
 $(document).ready(function() {
+  var dt = new Date();
+  var now = dt.getTime();
   chrome.tabs.getSelected(window.id, function (tab) {
-    if (! localStorage[tab.url]) {
-      var url = "http://b.hatena.ne.jp/entry/jsonlite/" + tab.url;
-     $("#dump").append('api access...');
-      console.log('api access...');
-      $.getJSON(url, function(json){
-        localStorage[tab.url] = JSON.stringify(json); // set cache
-//      $("#dump").append($.dump(json));
-        displayComment(json.bookmarks);
-      });
+    var cache = localStorage[tab.url];
+    //     $("#dump").append(now);
+    //     $("#dump").append('<br>');
+    //     $("#dump").append(JSON.parse(cache).createtd); // get cache
+    if (cache && JSON.parse(cache).createtd > now - cacheMSec) {
+      var json = JSON.parse(cache); // get cache
+      //      $("#dump").append($.dump(json));
+      //       console.log(json);
+      displayComment(json.bookmarks);
     }
     else {
-      var json = JSON.parse(localStorage[tab.url]); // get cache
-      console.log(json);
-      displayComment(json.bookmarks);
+      var url = "http://b.hatena.ne.jp/entry/jsonlite/" + tab.url;
+      $("#dump").append('api access...');
+      //       console.log('api access...');
+      $.getJSON(url, function(json){
+        json.createtd = now;
+        //         $("#dump").append($.dump(json));
+        localStorage[tab.url] = JSON.stringify(json); // set cache
+        displayComment(json.bookmarks);
+      });
     }
   });
 });
